f6a0305beb191985b58a5004b33afbb470f08396
manchangfengxu <1848953550@qq.com>
2023-11-20 21:24:01 +0800

    1th homework

--- homework/第一周的重构作业/main.go
+++ homework/第一周的重构作业/main.go
@@ -7,49 +7,6 @@ import (
 	"net/http"
 )
 
-func mmd(library []Login,c *gin.Context)(b []Login){
-	a, err := sonic.Marshal(&library)
-	if err != nil {
-		fmt.Print("22")
-	}
-
-	err = sonic.Unmarshal(a, &b)
-	if err != nil {
-		fmt.Print("22")
-	}
-
-	fmt.Println(b)
-	return b
-}
-
-func ChangeUser0(library []Login, c *gin.Context) {
-	library[0].Password = "456"
-	library[0].User = "q"
-
-	c.JSON(200, gin.H{"message": mmd(library,c),})
-}
-
-func DeleteUser0(library []Login, c *gin.Context) {
-	library1 := library[1:]
-
-	c.JSON(200, gin.H{"message": mmd(library1,c),})
-}
-
-func GetUser(library []Login, c *gin.Context) {
-	c.JSON(200, gin.H{"message": mmd(library,c)})
-}
-
-func LoginPost(c *gin.Context){
-	var json3 Login
-		if c.ShouldBind(&json3) == nil {
-			if json3.User == "ylx" && json3.Password == "123" {
-				c.JSON(http.StatusOK, gin.H{"status": "LOGIN"})
-			} else {
-				c.JSON(401, gin.H{"status": "UNLOGIN"})
-			}
-		}
-}
-
 type Login struct {
 	User     string `json:"user" binfing:"required"`
 	Password string `json:"password" binding:"required"`
@@ -65,7 +22,7 @@ func main() {
 
 	r.GET("/GetUser", func(c *gin.Context) { GetUser(library, c) })
 
-	r.POST("/LoginUser", func(c *gin.Context) {LoginPost(c)})
+	r.POST("/LoginUser", func(c *gin.Context) { LoginPost(c) })
 
 	r.PUT("/ChangeUser0", func(c *gin.Context) { ChangeUser0(library, c) })
 
@@ -73,4 +30,45 @@ func main() {
 
 	r.Run(":8080") // 监听并在 0.0.0.0:8080 上启动服务
 
+}
+
+func mmd(library []Login, c *gin.Context) (b []Login) {
+	a, err := sonic.Marshal(&library)
+	if err != nil {
+		fmt.Print("error!")
+	}
+
+	err = sonic.Unmarshal(a, &b)
+	if err != nil {
+		fmt.Print("error!")
+	}
+
+	fmt.Println(b)
+	return b
+}
+
+func ChangeUser0(library []Login, c *gin.Context) {
+	library[0].Password = "456"
+	library[0].User = "q"
+	c.JSON(http.StatusOK, gin.H{"message": mmd(library, c)})
+}
+
+func DeleteUser0(library []Login, c *gin.Context) {
+	library1 := library[1:]
+	c.JSON(http.StatusOK, gin.H{"message": mmd(library1, c)})
+}
+
+func GetUser(library []Login, c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": mmd(library, c)})
+}
+
+func LoginPost(c *gin.Context) {
+	var json3 Login
+	if err := c.ShouldBind(&json3); err == nil {
+		if json3.User == "ylx" && json3.Password == "123" {
+			c.JSON(http.StatusOK, gin.H{"status": "LOGIN"})
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"status": "UNLOGIN"})
+		}
+	}
 }
